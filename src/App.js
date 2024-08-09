a3ed4fc784759d40658addf24fb9d1370f2fb157
leosabinghimire <sabinghimire2055@gmail.com>
2024-08-09 15:20:16 +0545

    New UI Feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import Plan from "./Components/Plan/Plan";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
       <Hero />
+      <Plan />
     </div>
   );
 }
---
+++ src/Components/Plan/Plan.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./plan.css";
+
+const Plan = () => {
+  return (
+    <section>
+      <div className="plan">
+        <h2>Plan Your Perfect Trip</h2>
+        <p>Search Flights & Places Hire to our most popular destinations</p>
+      </div>
+
+      <div>hello world</div>
+    </section>
+  );
+};
+
+export default Plan;
